21d9d113a0e663b3d63d0baa0f3f11c36ac3cc30
Dr Richard Kiddle <RKiddle@users.noreply.github.com>
2025-02-05 15:18:19 +0700

    Add files via upload

---
+++ Turtle/turtle-triangle.py
@@ -0,0 +1,13 @@
+Python 3.12.8 (tags/v3.12.8:2dc476b, Dec  3 2024, 19:30:04) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import turtle
+>>> turtle.forward(15)
+>>> from turtle import *
+>>> forward(100)
+>>> left(120)
+>>> forward(100)
+... left(120)
+SyntaxError: multiple statements found while compiling a single statement
+>>> forward(100)
+>>> left(120)
+>>> forward(100)
